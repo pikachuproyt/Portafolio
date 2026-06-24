750748f867e58fdc69219dc949d04c293e84cce0
pikachuproyt <iker10alexandre@gmail.com>
2026-06-23 18:48:12 -0600

    Añadi javascript al proyecto

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    //1. Buscamos el parrafo en la bibliografia
+    const parrafo = document.querySelector('.info.seccion p');
+
+    //2. Guardamos el texto original y limpiamos el parrafo para empezar desde de cero
+    const textoOriginal = textoBiografia.textContent;
+    textoBiografia.textContent = '';
+    let indice  = 0;
+
+    //3. Funcion que escribe letra por letra
+    function escribirTexto() {
+        if (indice < textoOriginal.length){
+            textoBiografia.textContent += textoOriginal.charAt(indice);
+            indice++;
+            // Ajustamos el numero en milisegundos para cambiar la velocidad de escritura 
+            setTimeout(escribirTexto, 35);
+        }
+    }
+    //4.  Arranca la animacion despues de medio segundo de haber cargado la pagina
+    setTimeout(escribirTexto, 500);
+});
